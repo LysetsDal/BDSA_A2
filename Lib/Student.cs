01d0a841eddcb47faf539bdb74547b23fa2641b7
asger1278 <asly@itu.dk>
2022-09-17 08:49:22 +0200

    Student class

---
+++ Lib/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace Lib;
+public class Student
+{
+    public int Id { get; init; }
+
+    public string GivenName { get; set; }
+
+    public string SurName { get; set; }
+
+    public enum Status
+    {
+        New,
+        Active,
+        Dropout,
+        Graduated
+    }
+
+    public DateTime StartDate { get; set; } 
+
+    public DateTime EndDate { get; set; }
+
+    public DateTime GraduationDate { get; set; }
+
+
+}
