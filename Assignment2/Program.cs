fee2ddca5435dfb27c5f0e106841395f933ad463
asger1278 <asly@itu.dk>
2022-09-20 09:51:40 +0200

    Implemented cstudent

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,4 +1,10 @@
-﻿foreach (var wizard in WizardCollection.Create())
-{
-    Console.WriteLine(wizard);
-}
+﻿// foreach (var wizard in WizardCollection.Create())
+// {
+//     Console.WriteLine(wizard);
+// }
+
+var student = new Student(1, "Nicolai", "Friis");
+
+WriteLine(student.ToString());
+
+WriteLine(Student.Status.Active);
