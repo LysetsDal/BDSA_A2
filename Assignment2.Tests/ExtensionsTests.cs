95a2b5ef5a969cb444e1f6b54f054152028c0215
Turið Højgaard <tuho@itu.dk>
2022-09-20 15:32:09 +0200

    Implemented method and test for WordCount

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -14,4 +14,16 @@ public class ExtensionsTests
        // Assert
        Assert.Equal(true, result);
     }
+
+    [Fact]
+    public void WordCount_WhenGivenSentence() {
+        // Arrange
+        string sut = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+        
+        // Act
+        var result = Extensions.WordCount(sut);
+        
+        // Assert       
+        Assert.Equal(68, result);
+    }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -1,11 +1,23 @@
 namespace Assignment2;
 
+using System.Text.RegularExpressions;
+
+
 public static class Extensions
 {
     public static bool IsSecure(this Uri uri)
     {
         return uri.Scheme == Uri.UriSchemeHttps;
     }
-    
+
+    public static int WordCount(this string word) {
+        //only unicode letters (no numbers or symbols)
+        /*var input = word.Split(" ");
+        
+        return input.Length;*/
+
+        MatchCollection col = Regex.Matches(word, @"[\S]+");
+        return col.Count;
+    }
 
 }
